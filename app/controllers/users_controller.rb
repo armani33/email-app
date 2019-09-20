e13e7e6d4ec0b88e196fc8737e64cd0f39b1e765
armand chariot <armand@chariot.be>
2019-09-20 21:34:28 +0200

    genereate controller - model - view and put content in html file as well as doing routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,49 @@
+class UsersController < ApplicationController
+  before_action :set_variant, only: [:new]
+  def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html(&:phone)
+      # do |html|
+      # html.phone
+      # html.tablet
+      # end
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Thanks. Enjoy"
+      redirect_to root_path
+    else
+      # render :new => ca c'est si on veut rediriger vers une def local
+      flash[:error] = "something went wrong"
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name)
+  end
+
+  def set_variant
+    case request.user_agent
+    when /iPhone/i
+      request.variant = :phone
+    when /Android/i && /mobile/i
+      request.variant = :phone
+    when /Windows Phone/i
+      request.variant = :phone
+    # when /Android/i
+    #   request.variant = :tablet
+    # when /iPad/i
+    #   request.variant = :tablet
+    end
+  end
+
+end
